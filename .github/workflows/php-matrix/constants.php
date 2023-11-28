4b8486e58a9eb1cb318a80961f7fb43636e94fd3
Rick Bouma <r.bouma@epartment.nl>
2023-11-28 18:20:57 +0100

    fix build for composer1 images

--- .github/workflows/php-matrix/constants.php
+++ .github/workflows/php-matrix/constants.php
@@ -1,8 +1,15 @@
 <?php
 
 const PHP_LATEST = '8.2';
+
+/*
+* Don't include older builds because these are already build and not going to be updated from official repo
+* If there is something changed in our Dockerfile and you want to build all versions then define it as follows:
+* const PHP_VERSIONS = ['7.1', '7.2', '7.3', '7.4', '8.0', '8.1', '8.2'];
+*/
 const PHP_VERSIONS = ['7.1', '7.2', '7.3', '7.4', '8.0', '8.1', '8.2'];
-CONST PHP_VERSIONS_OS_RELEASE = [
+//const PHP_VERSIONS = ['8.2'];
+const PHP_VERSIONS_OS_RELEASE = [
     '7.1' => 'buster',
     '7.2' => 'buster',
     '7.3' => 'bullseye',
@@ -13,7 +20,7 @@ CONST PHP_VERSIONS_OS_RELEASE = [
 ];
 const NODE_LATEST = '21';
 const NODE_VERSIONS = ['10', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21'];
-CONST NODE_VERSIONS_OS_RELEASE = [
+const NODE_VERSIONS_OS_RELEASE = [
     '10' => 'stretch',
     '12' => 'bullseye',
     '13' => 'buster',
@@ -28,4 +35,3 @@ CONST NODE_VERSIONS_OS_RELEASE = [
 ];
 const EXPERIMENTAL_PHP_VERSIONS = [];
 const NOT_STABLE_XDEBUG_PHP_VERSIONS = ['7.0', '7.1', '7.2', '7.3', '7.4'];
-

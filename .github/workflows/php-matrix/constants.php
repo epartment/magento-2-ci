e4edcb56863c0eac795fd6148b766ddd03ed8f5c
Henk-Roel van Zijl <h.vanzijl@epartment.nl>
2025-01-14 16:35:03 +0100

    Added node alpine and removed EOL Node versions

--- .github/workflows/php-matrix/constants.php
+++ .github/workflows/php-matrix/constants.php
@@ -21,6 +21,7 @@ const PHP_VERSIONS_OS_RELEASE = [
 const NODE_LATEST = '21';
 const NODE_VERSIONS = ['16', '18', '19', '20', '21', '22'];
 const NODE_VERSIONS_OS_RELEASE = [
+    '16' => 'bullseye',
     '18' => 'bullseye',
     '19' => 'bullseye',
     '20' => 'bookworm',
